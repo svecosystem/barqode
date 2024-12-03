28ba09a2c0ab2cb90597c55645966ff02ba11377
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-12-03 12:34:01 +0100

    export all props

--- packages/barqode/src/lib/index.ts
+++ packages/barqode/src/lib/index.ts
@@ -2,4 +2,11 @@ export { default as BarqodeCapture } from "./components/barqode-capture.svelte";
 export { default as BarqodeDropZone } from "./components/barqode-drop-zone.svelte";
 export { default as BarqodeStream } from "./components/barqode-stream.svelte";
 
-export type { DetectedBarcode, BarcodeFormat, Point2D, StreamProps } from "./components/types.js";
+export type {
+	DetectedBarcode,
+	BarcodeFormat,
+	Point2D,
+	CaptureProps,
+	DropZoneProps,
+	StreamProps,
+} from "./components/types.js";
